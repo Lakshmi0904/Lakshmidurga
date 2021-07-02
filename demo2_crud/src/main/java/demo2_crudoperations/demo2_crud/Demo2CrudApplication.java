25d69be77e71570e813d3dc64d0b1a25fa358e62
Laxmi durga <Laxmi durga@LAPTOP-OFV0774I>
2021-07-02 07:41:09 +0530

    Spring jpa crud.

---
+++ demo2_crud/src/main/java/demo2_crudoperations/demo2_crud/Demo2CrudApplication.java
@@ -0,0 +1,13 @@
+package demo2_crudoperations.demo2_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2CrudApplication.class, args);
+	}
+
+}
---
+++ demo2_crud/src/test/java/demo2_crudoperations/demo2_crud/Demo2CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package demo2_crudoperations.demo2_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
